e2d0c02f682d264f2143868d90bc55122f66f749
Dawid Wdowiak <dawidwdowiak@hotmail.com>
2019-10-08 17:47:16 +0200

    Added UGlobalHotkey library

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,25 @@
 #include <QApplication>
 #include "mainwindow.h"
 
+#include "uglobalhotkeys.h"
+#include <QDebug>
+
+
 int main(int argc, char *argv[])
 {
 	int result{};
 	{
 		QApplication a(argc, argv);
 
+		// teest
+		UGlobalHotkeys* hotkeyManager = new UGlobalHotkeys();
+		hotkeyManager->registerHotkey("Ctrl+Shift+F11 ");
+		QObject::connect(hotkeyManager, &UGlobalHotkeys::activated, [=](size_t id)
+		{
+			qDebug() << "Activated: " << QString::number(id);
+		});
+
+		
 		MainWindow window;
 		window.show();
 
